5f4b7236dfc892d392929e911f30d721f731ba20
hao0731 <ss501801@gmail.com>
2022-02-19 19:46:11 +0800

    feat: initial mongoose config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import databaseConfig from './configs/database.config';
@@ -10,6 +11,13 @@ import secretConfig from './configs/secret.config';
     ConfigModule.forRoot({
       isGlobal: true,
       load: [databaseConfig, secretConfig],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('database.uri'),
+      }),
     }),
   ],
   controllers: [AppController],
