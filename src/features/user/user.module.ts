6e5e9260eb51ccc120cdd39a807c9eae90d163e1
hao0731 <ss501801@gmail.com>
2022-02-21 22:49:17 +0800

    feat(feature:user): add default admin

--- src/features/user/user.module.ts
+++ src/features/user/user.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { User, UserSchema } from '../../core/models/user';
+
 import { UserService } from './user.service';
 
 @Module({
-  providers: [UserService]
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
--- src/features/user/user.service.ts
+++ src/features/user/user.service.ts
@@ -1,4 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
+
+import { Model } from 'mongoose';
+
+import * as bcrypt from 'bcrypt';
+
+import { User, UserDocument, Role } from '../../core/models/user';
+
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService implements OnApplicationBootstrap {
+  constructor(
+    private readonly configService: ConfigService,
+    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  async onApplicationBootstrap() {
+    await this.createDefaultAdmin();
+  }
+
+  public async createUser(dto: CreateUserDto) {
+    const { password } = dto;
+    const hash = await bcrypt.hash(password, 12);
+    return this.userModel.create({ ...dto, password: hash });
+  }
+
+  private async createDefaultAdmin() {
+    const { username, password, email } = this.configService.get('admin');
+    const dto: CreateUserDto = {
+      username,
+      password,
+      email,
+      role: Role.ADMIN,
+    };
+    const exist = await this.userModel
+      .exists({
+        $and: [{ username }, { role: Role.ADMIN }],
+      })
+      .exec();
+    if (exist) {
+      return;
+    }
+    await this.createUser(dto);
+  }
+}
