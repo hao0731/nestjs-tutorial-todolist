829b8fad95f6201059319179d140223afc83d259
hao0731 <ss501801@gmail.com>
2022-02-23 22:24:29 +0800

    feat(common): add role guard

--- src/common/guards/role.guard.ts
+++ src/common/guards/role.guard.ts
@@ -1,11 +1,28 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+
+import { Request } from 'express';
 import { Observable } from 'rxjs';
+
+import { AuthorizationService } from '../modules/authorization';
+
+import { IUserPayload } from '../../features/auth';
 
 @Injectable()
 export class RoleGuard implements CanActivate {
+  constructor(private readonly authorizationService: AuthorizationService) {}
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+    const ctx = context.switchToHttp();
+    const request = ctx.getRequest<Request>();
+    const { user, method, path } = request;
+    const { role } = user as IUserPayload;
+    const action = this.authorizationService.mappingAction(method);
+    return this.authorizationService.checkPermission(
+      `role:${role}`,
+      path,
+      action,
+    );
   }
 }
