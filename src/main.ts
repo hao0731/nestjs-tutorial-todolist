ec30e6ce97e19f39c5f0ae17e8c2d951c98054be
hao0731 <ss501801@gmail.com>
2022-02-27 20:44:52 +0800

    feat: setup swagger UI

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerCustomOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setupSwagger(app);
   await app.listen(3000);
 }
+
+function setupSwagger(app: INestApplication) {
+  const builder = new DocumentBuilder();
+  const config = builder
+    .setTitle('TodoList')
+    .setDescription('NestJS tutorial')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  const options: SwaggerCustomOptions = { explorer: true };
+  SwaggerModule.setup('api', app, document, options);
+}
+
 bootstrap();
