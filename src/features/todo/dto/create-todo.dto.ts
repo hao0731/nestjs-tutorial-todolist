6d16647fab3eff63dccd63ebe688d541c4cd7646
hao0731 <ss501801@gmail.com>
2022-02-27 20:46:00 +0800

    feat(features:todo): add swagger settings

--- src/features/todo/dto/create-todo.dto.ts
+++ src/features/todo/dto/create-todo.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsBoolean,
   IsNotEmpty,
@@ -13,18 +14,35 @@ import {
 } from './../../../core/models/todo';
 
 export class CreateTodoDto {
+  @ApiProperty({
+    minLength: TODO_TITLE_MIN_LENGTH,
+    maxLength: TODO_TITLE_MAX_LENGTH,
+    description: 'todo 的標題',
+  })
   @MinLength(TODO_TITLE_MIN_LENGTH)
   @MaxLength(TODO_TITLE_MAX_LENGTH)
   public readonly title: string;
 
+  @ApiProperty({
+    maxLength: TODO_DESCRIPTION_MAX_LENGTH,
+    required: false,
+    description: 'todo 的詳細內容',
+  })
   @IsOptional()
   @MaxLength(TODO_DESCRIPTION_MAX_LENGTH)
   public readonly description?: string;
 
+  @ApiProperty({
+    required: false,
+    description: 'todo 是否完成',
+  })
   @IsOptional()
   @IsBoolean()
   public readonly completed?: boolean;
 
+  @ApiProperty({
+    description: '被指派任務的 user 的 id',
+  })
   @IsNotEmpty()
   public readonly assignee: string;
 }
--- src/features/todo/todo.controller.ts
+++ src/features/todo/todo.controller.ts
@@ -9,6 +9,13 @@ import {
   Query,
   UseGuards,
 } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
 
 import { JwtGuard, RoleGuard } from '../../common/guards';
 
@@ -16,17 +23,27 @@ import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
 import { TodoService } from './todo.service';
 
+@ApiTags('Todo')
+@ApiBearerAuth()
 @UseGuards(JwtGuard, RoleGuard)
 @Controller('todos')
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
 
+  @ApiCreatedResponse({
+    description: '成功建立 todo',
+  })
   @Post()
   async createTodo(@Body() dto: CreateTodoDto) {
     const todo = await this.todoService.createTodo(dto);
     return todo.toJSON();
   }
 
+  @ApiOkResponse({
+    description: '取得 todo 列表',
+  })
+  @ApiQuery({ name: 'skip', required: false })
+  @ApiQuery({ name: 'limit', required: false })
   @Get()
   async getTodos(@Query('skip') skip: number, @Query('limit') limit: number) {
     const documents = await this.todoService.getTodos(skip, limit);
@@ -34,18 +51,27 @@ export class TodoController {
     return todos;
   }
 
+  @ApiOkResponse({
+    description: '取得 todo',
+  })
   @Get(':id')
   async getTodo(@Param('id') id: string) {
     const todo = await this.todoService.getTodoById(id);
     return todo ? todo.toJSON() : {};
   }
 
+  @ApiOkResponse({
+    description: '成功更新 todo',
+  })
   @Patch(':id')
   async updateTodo(@Param('id') id: string, @Body() dto: UpdateTodoDto) {
     const todo = await this.todoService.updateTodo(id, dto);
     return todo.toJSON();
   }
 
+  @ApiOkResponse({
+    description: '成功刪除 todo',
+  })
   @Delete(':id')
   async removeTodo(@Param('id') id: string) {
     await this.todoService.removeTodo(id);
